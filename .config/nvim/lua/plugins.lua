3c9b321f22e0f0bea8d98be4330d2389310b94ea
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-12-19 00:52:31 +0700

    update nvim plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,19 +1,10 @@
-local vim = vim
-local execute = vim.api.nvim_command
 local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
--- ensure that packer is installed
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-vim.cmd('packadd packer.nvim')
-local packer = require'packer'
-local util = require'packer.util'
-
-packer.init({
-  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
-})
-
--- startup and add configure plugins
-packer.startup(function()
+return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'tpope/vim-fugitive'
   use 'wincent/terminus'
