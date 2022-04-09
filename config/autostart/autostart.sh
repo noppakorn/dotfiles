9c02aabdccbe6c1c9290c5c40ab33f06d7ae8b0c
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-04-09 16:35:44 +0700

    picom and autostart

--- config/autostart/autostart.sh
+++ config/autostart/autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Startup program
 
-xset -dpms s off
+# xset -dpms s off
+xautolock -notify 30 -notifier "notify-send -u critical -t 10000 'LOCKING in 30s'" -time 10 -locker "systemctl suspend" -corners "--00" &
 
 setxkbmap -model pc105 -layout us,th -option grp:win_space_toggle -option caps:escape &
 
@@ -12,6 +13,4 @@ autorandr --change
 feh --bg-fill ~/Pictures/beautiful-morning-minimal-4k-if.jpg
 flameshot &
 
-# export PATH=~/.local/share/gem/ruby/3.0.0/bin:$PATH
-# ~/.local/share/gem/ruby/3.0.0/bin/fusuma --config /home/noppakorn/.config/fusuma/config.yml -d &
 (sleep 5 && /home/noppakorn/map_touchscreen.sh) &
