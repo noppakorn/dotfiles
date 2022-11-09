2d44c74371c746a65ea7f4df4029d8b7313aa0b9
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-11-09 18:24:38 +0700

    update autostart

--- config/autostart/autostart.sh
+++ config/autostart/autostart.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 # Startup program
 
+# Keyboard Mapping
+setxkbmap -model pc105 -layout us,th -option grp:win_space_toggle -option caps:escape -option compose:menu &
+
+# Do not turn off display
 # xset -dpms s off
-xautolock -notify 30 -notifier "notify-send -u critical -t 10000 'LOCKING in 30s'" -time 10 -locker "systemctl suspend" -corners "--00" &
 
-setxkbmap -model pc105 -layout us,th -option grp:win_space_toggle -option caps:escape &
+## Lockscreen
+# Update betterlockscreen cache
+betterlockscreen -u ~/Pictures/beautiful-morning-minimal-4k-if.jpg --fx &
+xidlehook --not-when-fullscreen --not-when-audio --timer 830 "notify-send -u critical -t 10000 'LOCKING in 30s'" "" --timer 30 "systemctl suspend" "" &
+xss-lock --transfer-sleep-lock -- betterlockscreen -l &
 
+# Startup Programs
 lxqt-policykit-agent &
 picom --experimental-backends &
 parcellite &
-autorandr --change
-feh --bg-fill ~/Pictures/beautiful-morning-minimal-4k-if.jpg
 flameshot &
+killall pasystray && pasystray &
+autorandr --change
+nitrogen --restore
 
-(sleep 5 && /home/noppakorn/map_touchscreen.sh) &
+# Touchpad Gestures
+libinput-gestures-setup start &
+# Touchscreen Mapping
+(sleep 5 && /home/noppakorn/.local/bin/map_touchscreen) &
